4e2982424984b46b0ec471608c58b6482b1c0769
sasikad1 <sasikadilum40@gmail.com>
2024-02-29 16:35:12 +0530

    hotel-management-system

--- src/edu/ijse/coursework2/entity/RoomCategoryEntity.java
+++ src/edu/ijse/coursework2/entity/RoomCategoryEntity.java
@@ -4,19 +4,23 @@
  */
 package edu.ijse.coursework2.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.annotations.Entity;
 
 /**
  *
  * @author sasik
  */
-@javax.persistence.Entity
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -24,9 +28,18 @@ import org.hibernate.annotations.Entity;
 @ToString
 @Table(name="RoomCategory")
 public class RoomCategoryEntity {
+    @Id
+    @Column(name="room_cate_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer room_cate_id;
+    
+    @Column(name="room_cate_name", nullable = false,length = 30)
     private String room_cate_name;
+    
+    @Column(name="room_cate_description", nullable = false,length = 50)
     private String room_cate_description;
+    
+    @Column(name="room_cate_amount", nullable = false,length = 30)
     private Integer room_cate_amount;
 
     public RoomCategoryEntity(String room_cate_name, String room_cate_description, Integer room_cate_amount) {
@@ -34,4 +47,6 @@ public class RoomCategoryEntity {
         this.room_cate_description = room_cate_description;
         this.room_cate_amount = room_cate_amount;
     }
+    
+    
 }
